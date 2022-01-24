df95e393d520a20e2615fb4a8a29cbc479ca8f31
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:15:18 +0300

    Update itc_sum_even_lst.cpp

--- itc_sum_even_lst.cpp
+++ itc_sum_even_lst.cpp
@@ -1,9 +1,13 @@
 #include "middle_list.h"
 
 long itc_sum_even_lst(const vector <int> &lst){
+
+if (lst.size() != 0){
+
 long itog = 0;
 for(long long i = 0; i < lst.size(); i++)
     if (i % 2 == 0)
         itog += lst[i];
+}
 return itog;
 }
