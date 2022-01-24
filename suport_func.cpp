ea25d9c2785a960f2fc55d6925c9fdae15d8fefd
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:53:58 +0300

    Update suport_func.cpp

--- suport_func.cpp
+++ suport_func.cpp
@@ -1,5 +1,15 @@
 #include "middle_list.h"
 
+long long itc_len(string str){
+    long long kol = 0, i = 0;
+    while (str[i] != '\0'){
+        kol++;
+        i++;
+    }
+   return kol;
+}
+
+
 int itc_find_str(string str1, char str2){
     long long len1 = 0;
     len1 =  itc_len(str1);
