b918c4de8567d358498997ad8f34af363a9103ec
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:16:20 +0300

    Update itc_sum_even_part_lst.cpp

--- itc_sum_even_part_lst.cpp
+++ itc_sum_even_part_lst.cpp
@@ -2,8 +2,12 @@
 
 long itc_sum_even_part_lst(const vector <int> &lst){
 long itog = 0;
+
+if (lst.size() != 0){
+
 for(long long i = 0; i < lst.size(); i++)
     if (lst[i] % 2 == 0)
         itog += lst[i];
+}
 return itog;
 }
