8408134a9402f8f66929264a44f4003396cf0f3c
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:13:31 +0300

    Update itc_strtlist.cpp

--- itc_strtlist.cpp
+++ itc_strtlist.cpp
@@ -1,7 +1,8 @@
 #include "middle_list.h"
  vector <char> itc_strtlist(string str){
-     vector <char> mass;
+     vector <char> mass(itc_len(str));
      for(int i = 0; str[i] != '\0'; i++)
-        mass.push_back(str[i]);
+         mass.push_back(str[i]);
      return mass;
+
  }
