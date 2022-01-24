2e977d8e54b2ad825963275bf9ceeced48a0ccc4
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:58:47 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -1,18 +1,22 @@
+
 #include "middle_list.h"
 
 string itc_rmstrchar(string str, string less1){
-    string itog = "", sup = "";
-
-if (less1 == "")
-    return str;
-if (less1 == str || str == "")
-    return "";
-
-    for (int i = 0; str[i] != '\0'; i++){
-        sup += str[i];
-        if (itc_find_str(less1, sup) == -1)
-            itog += str[i];
-        sup = "";
+    string new_str = str;
+    int start = 0, ind = 0;
+    int lon = itc_len(less1);
+    if (less1 == "")
+        return str;
+    if (less1 == str)
+        return "";
+    for (int i = 0; i < lon; i++){
+        ind = itc_find_str(str, less1[i]);
+    while(ind != -1){
+        new_str = itc_slice_str(str, start, ind - 1);
+        new_str += itc_slice_str(str, ind + 1, itc_len(str));
+        ind = itc_find_str(new_str, less1[i]);
+        str = new_str; 
     }
-    return itog;
+    }
+    return new_str;
 }
