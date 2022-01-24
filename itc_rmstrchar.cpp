a99c06032b6184954d3ae88e87ad235ca4e14403
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 11:59:24 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -2,6 +2,12 @@
 
 string itc_rmstrchar(string str, string less1){
     string itog = "", sup = "";
+
+if (less1 == "")
+    return str;
+if (less1 == str || str == "")
+    return "";
+
     for (int i = 0; str[i] != '\0'; i++){
         sup += str[i];
         if (itc_find_str(less1, sup) == -1)
