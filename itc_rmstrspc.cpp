9d3a06eb6934686cfbb41224e4221df1216c2c3b
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:59:38 +0300

    Update itc_rmstrspc.cpp

--- itc_rmstrspc.cpp
+++ itc_rmstrspc.cpp
@@ -1,10 +1,10 @@
 #include "middle_list.h"
 
- string itc_rmstrspc(string str){
- string itog = "";
- for(long long i = 0; i < itc_len(str); i++){
-    if (str[i] != 32)
-        itog += str[i];
- }
-return itog;
+string itc_rmstrspc(string str){
+    string new_str = "";
+    for(long long i = 0; i < itc_len(str); i++){
+        if (str[i] != 32)
+            new_str += str[i];
+    }
+    return new_str;
 }
