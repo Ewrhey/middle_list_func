7491b5d7a19cea874b6caf99aced995f378a4f74
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:47:36 +0300

    Update itc_join.cpp

--- itc_join.cpp
+++ itc_join.cpp
@@ -1,17 +1,14 @@
+
 #include "middle_list.h"
- string itc_join(vector <char> lst, string sep){
-string str = "";
-if (lst.size() > 0){
 
-    
-     for(int i = 0; i < lst.size() - 1; i++){
-        str += lst[i];
-        str += sep;
-     }
-
-str += lst[lst.size() - 1];
-
+string itc_join(vector <char> lst, string sep){
+    string new_str = "";
+    if (lst.size() > 0){
+    for(int i = 0; i < (lst.size() - 1); i++){
+        new_str += lst[i];
+        new_str += sep;
+    }
+    new_str += lst[lst.size() - 1];
+    }
+    return new_str;
 }
-     return str;
- }
-
