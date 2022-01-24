68688dcaf0656747507af34be9669770fdff4a86
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-24 12:17:33 +0300

    Update itc_sumlst.cpp

--- itc_sumlst.cpp
+++ itc_sumlst.cpp
@@ -1,8 +1,13 @@
 #include "middle_list.h"
 long itc_sumlst(const vector <int> &lst){
 long itog = 0;
+
+if (lst.size() != 0){
+
 for(long long i = 0; i < lst.size(); i++)
     itog += lst[i];
 return itog;
+
 }
 
+}
